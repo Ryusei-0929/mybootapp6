6f85b0eb96266c9e94a9fa9bec72bab4a3a7b34d
Ryusei-0929 <220033@jc-21.jp>
2025-06-02 10:25:00 +0900

    メッセージ

--- src/main/test/java/com/example/mybootapp7/Mybootapp7ApplicationTests.java
+++ src/main/test/java/com/example/mybootapp7/Mybootapp7ApplicationTests.java
@@ -1,13 +1,12 @@
-package com.example.mybootapp6;
+package jp.te4a.spring.boot.myapp7.mybootapp7;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class Mybootapp6ApplicationTests {
+class Mybootapp7ApplicationTests {
 
 	@Test
 	void contextLoads() {
 	}
-
 }
